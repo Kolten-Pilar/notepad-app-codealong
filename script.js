8e603e980179f86b87acb9d2683d35dc21f0f333
Kolten ✔️ <kolten.pilar@gmail.com>
2023-06-21 15:32:15 -0400

    completed

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const addBtn = document.getElementById('add');
 
-addBtn.addEventListener('click', ()=> addNewNote('Add New Note'));
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes) {
+  notes.forEach(note => addNewNote(note));
+}
+
+addBtn.addEventListener('click', ()=> addNewNote());
 
 function addNewNote(text = '') {
   const note = document.createElement('div');
@@ -40,5 +46,25 @@ function addNewNote(text = '') {
     textArea.classList.toggle('hidden');
   })
 
+  textArea.addEventListener('input', (e)=>{
+    const value = e.target.value
+    main.innerHTML = value;
+
+    updateLS();
+  })
+
   document.body.appendChild(note);
+}
+
+
+function updateLS() {
+  const notesText = document.querySelectorAll('textarea');
+
+  const notes = [];
+
+  notesText.forEach(note => notes.push(note.value));
+
+  // console.log(notes, notesText);
+
+  localStorage.setItem('notes', JSON.stringify(notes));
 }
